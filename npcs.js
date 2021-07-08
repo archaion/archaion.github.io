3ceeace026d2266629b520f9ee6164984ca01e7d
NE0N8 <80986857+NE0N8@users.noreply.github.com>
2021-07-08 10:48:15 -0400

    Delete npcs.js

--- npcs.js
+++
@@ -1,6 +0,0 @@
-
-////////////////////////// ENGINE USE ONLY //////////////////////////
-
-var NPCs = [
-   new Character(2, "Enemy", "Asian", "Soldier", 1, {})   // REPLACE "ID" ARG WITH "UID" OF ENGINE OBJECT
-];
